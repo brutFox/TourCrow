01120e5ce46a13a0702a71cb75a33b8689c0652f
brutfox <rafat-ahmad2live.com>
2015-12-16 03:07:05 +0600

    kn

--- TourCrow/Scripts/SearchPage.js
+++ TourCrow/Scripts/SearchPage.js
@@ -21,7 +21,7 @@ $("#submit").click(function () {
 
 function makeSuggPlace(placeId, name, photo, ratting, index) {
     //placeId = placeId.replace("-", "/");
-    var retData = "<div class='sugg-element'> <div class='main-area-for-element'><img src='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photo + "&key=AIzaSyD7FknkbSmCxMXsWnnCcL106f94KKrKA2Q' alt=''/> <p>" + name + "</p> <p>Ratting : " + ratting + "</p><div class='action-for-sugg'><input type='button' value='Add' onclick=\"addItemToPackage(" + index + ")\"class='add-to-list'/><br/><input type='button' value='Remove' class='remove-from-list'/> </div></div></div>";
+    var retData = "<div class='sugg-element'> <div class='main-area-for-element'><img src='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photo + "&key=AIzaSyAX1EHCUo6oibCxw3gKDuot3r6B-2wrm2s' alt=''/> <p>" + name + "</p> <p>Ratting : " + ratting + "</p><div class='action-for-sugg'><input type='button' value='Add' onclick=\"addItemToPackage(" + index + ")\"class='add-to-list'/><br/><input type='button' value='Remove' class='remove-from-list'/> </div></div></div>";
     return retData;
 }
 
@@ -33,7 +33,7 @@ function addItemToPackage(index) {
 
 function plan_div(place_photo, place_name, place_address, place_id, index) {
 
-    var inside_div = "<div class='plan-single-element' id='" + place_id + "'><input type='hidden' name='pid' value='"+ place_id +"'><img src='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + place_photo + "&key=AIzaSyD7FknkbSmCxMXsWnnCcL106f94KKrKA2Q' alt=''/><div class='desc-elemetn'><h3>" + place_name + "</h3><p>" + place_address + "</p><div class='list-action-area'><select name='' class='move-to-days'><option value='0'>Move to day</option><option value='1'>Day 1</option><option value='2'>Day 2</option><option value='3'>Day 3</option></select><div class='remove-place'><span onclick = 'remove_view("+ index +")'>Remove</span></div><div class='more-info'><span>More Info</span></div></div></div><div class='clear'></div></div>";
+    var inside_div = "<div class='plan-single-element' id='" + place_id + "'><input type='hidden' name='pid' value='" + place_id + "'><img src='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + place_photo + "&key=AIzaSyAX1EHCUo6oibCxw3gKDuot3r6B-2wrm2s' alt=''/><div class='desc-elemetn'><h3>" + place_name + "</h3><p>" + place_address + "</p><div class='list-action-area'><select name='' class='move-to-days'><option value='0'>Move to day</option><option value='1'>Day 1</option><option value='2'>Day 2</option><option value='3'>Day 3</option></select><div class='remove-place'><span onclick = 'remove_view(" + index + ")'>Remove</span></div><div class='more-info'><span>More Info</span></div></div></div><div class='clear'></div></div>";
     
 
     return inside_div;
