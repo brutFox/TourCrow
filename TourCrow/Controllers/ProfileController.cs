26b6e840ad67cf9e4d4fa50ecebf2c53c28a9c91
brutfox <rafat-ahmad@live.com>
2015-12-23 13:47:34 +0600

    tourlist left panel done

--- TourCrow/Controllers/ProfileController.cs
+++ TourCrow/Controllers/ProfileController.cs
@@ -13,15 +13,26 @@ namespace TourCrow.Controllers
         //
         // GET: /Profile/
         TourCrowDBEntities tcdb = new TourCrowDBEntities();
-        //public string username=;
+        //public string username = null;
         //public string fbId = null;
-        
-
+        //public string packName = null;
+        //public string packName;
         [HttpGet]
         public ActionResult Index()
         {
             string username = Convert.ToString(Request["userName"]);
             string fbId = Convert.ToString(Request["fbId"]);
+            string packName;
+            DateTime now = DateTime.Now;
+
+            if (Request["packName"] == "")
+            {
+                packName = DateTime.Now.ToString();
+            }
+            else
+            {
+                packName = Convert.ToString(Request["packName"]);
+            }
 
             if (username.IsNullOrWhiteSpace())
             {
@@ -45,27 +56,27 @@ namespace TourCrow.Controllers
 
 
 
-            using (tcdb)
+            using (var context = new TourCrowDBEntities())
             {
                 //var query = from usr in tcdb.USERs where usr.UserFBID == fbId select usr;
                 //var chkusr = query.Count();
                 //Response.Write(chkusr);
                 try
                 {
-                    var query = from usr in tcdb.USERs where usr.UserFBID == fbId select usr;
+                    var query = from usr in context.USERs where usr.UserFBID == fbId select usr;
                     var chkusr = query.Count();
                     if (chkusr > 0 && pids.Length > 0)
                     {
-                        insertPackage(pids, fbId, username, photos);
+                        insertPackage(pids, fbId, username, photos, packName, now);
                     }
 
                     else if (chkusr <= 0 && pids.Length > 0)
                     {
                         var user_insert_query = new USER { UserName = username, UserFBID = fbId };
-                        tcdb.USERs.Add(user_insert_query);
-                        tcdb.SaveChanges();
+                        context.USERs.Add(user_insert_query);
+                        context.SaveChanges();
 
-                        insertPackage(pids, fbId, username, photos);
+                        insertPackage(pids, fbId, username, photos, packName, now);
                     }
                 }
                 catch (Exception)
@@ -80,13 +91,20 @@ namespace TourCrow.Controllers
 
         public void showPackages(string fbId_new)
         {
-            using (tcdb)
+            using (var packcon = new TourCrowDBEntities())
             {
-                var pck_date = from pck in tcdb.PACKAGEs where pck.UserFBID == fbId_new.ToString() select pck.Date;
-                var check_pack = pck_date.ToList();
-                ViewBag.count_pack = pck_date.Count();
+
+                var pck_name = from pck in packcon.PACKAGEs where pck.UserFBID == fbId_new.ToString() select pck.Title;
+                var check_pack_nm = pck_name.ToList();
+                ViewBag.count_pack = pck_name.Count();
+
+                ViewBag.packages = check_pack_nm;
+
+                //var pck_date = from pck in packcon.PACKAGEs where pck.UserFBID == fbId_new.ToString() select pck.Date;
+                //var check_pack = pck_date.ToList();
+                //ViewBag.count_pack = pck_date.Count();
                 
-                ViewBag.packages = check_pack;
+                //ViewBag.packages = check_pack;
             }
 
             //return ("Index");
@@ -138,23 +156,33 @@ namespace TourCrow.Controllers
         //    return View("Index");
         //}
 
-        private void insertPackage(string[] pids,string fbId,string username,string[] photos)
+        private void insertPackage(string[] pids,string fbId,string username,string[] photos, string packName, DateTime now)
         {
             //insert package
-            var pack_insert_query = new PACKAGE { UserFBID = fbId, Date = DateTime.Now};
-            tcdb.PACKAGEs.Add(pack_insert_query);
-            tcdb.SaveChanges();
-
-            //get package id
-            var get_packid = from pckid in tcdb.PACKAGEs where pckid.UserFBID == fbId.ToString() select pckid.PackageID;
-            var packid = get_packid.AsEnumerable().LastOrDefault();
-
-            
-            for (int i = 0, j = 0; i < pids.Count() && j < photos.Count(); i++, j++)
+            using (var dbcon = new TourCrowDBEntities())
             {
-                var userpack_insert_query = new USER_PACKAGE { PackageID = Convert.ToInt32(packid), PlaceID = pids[i], PhotoKey = photos[i]};
-                tcdb.USER_PACKAGE.Add(userpack_insert_query);
-                tcdb.SaveChanges();
+                
+                var pack_insert_query = new PACKAGE {UserFBID = fbId, Date = now, Title = packName};
+                dbcon.PACKAGEs.Add(pack_insert_query);
+                dbcon.SaveChanges();
+//( packName.ToString() == null ? Convert.ToString(DateTime.Now) : packName.ToString() )
+                //get package id
+                var get_packid = from pckid in dbcon.PACKAGEs
+                    where pckid.UserFBID == fbId.ToString()
+                    select pckid.PackageID;
+                var packid = get_packid.AsEnumerable().LastOrDefault();
+
+                for (int i = 0, j = 0; i < pids.Count() && j < photos.Count(); i++, j++)
+                {
+                    var userpack_insert_query = new USER_PACKAGE
+                    {
+                        PackageID = Convert.ToInt32(packid),
+                        PlaceID = pids[i],
+                        PhotoKey = photos[i]
+                    };
+                    dbcon.USER_PACKAGE.Add(userpack_insert_query);
+                    dbcon.SaveChanges();
+                }
             }
         }
 
