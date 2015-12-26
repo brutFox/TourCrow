640efca75366df676f3138e9dde868cce4f8189f
brutfox <rafat-ahmad@live.com>
2015-12-26 22:24:59 +0600

    changed a lot

--- TourCrow/Controllers/ProfileController.cs
+++ TourCrow/Controllers/ProfileController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using Microsoft.Ajax.Utilities;
+using TourCrow.App_Start;
 using TourCrow.Models;
 
 namespace TourCrow.Controllers
@@ -23,7 +28,12 @@ namespace TourCrow.Controllers
             string username = Convert.ToString(Request["userName"]);
             string fbId = Convert.ToString(Request["fbId"]);
             string packName;
+            int packId= Convert.ToInt32(Request["packid"]);
+        
             DateTime now = DateTime.Now;
+
+            ViewBag.name = username;
+            ViewBag.id = fbId;
 
             if (Request["packName"] == "")
             {
@@ -46,7 +56,7 @@ namespace TourCrow.Controllers
             ViewBag.username = username;
             ViewBag.fbID = fbId;
 
-            showPackages(fbId);
+            
 
             string email = Convert.ToString(Request["fbEmail"]);
             //Response.Write(email+"</br>");
@@ -86,7 +96,36 @@ namespace TourCrow.Controllers
 
             }
 
+            showPackages(fbId);
+            getPackageInfo(fbId,packId);
+
             return View();
+        }
+
+        private void getPackageInfo(String fbId, int packId)
+        {
+            using (var pack = new TourCrowDBEntities())
+            {
+                var pck_id = from p in pack.USER_PACKAGE where p.PackageID == packId select p.PlaceID;
+                var pck_count = pck_id.ToList();
+                ViewBag.pck_count = pck_id.Count();
+                List<string> pname = new List<string>();
+                List<string> padd = new List<string>();
+                List<string> pimg = new List<string>();
+                int i = 0;
+                foreach (var placeId in pck_count)
+                {
+                    String json = place_details(placeId);
+                    ProfilePageModel.RootObject model = parseData(json);
+                    pname.Add(model.result.name);
+                    padd.Add(model.result.formatted_address);
+                    pimg.Add(model.result.photos[0].photo_reference);
+                    i++;
+                }
+                ViewBag.pname = pname;
+                ViewBag.padd = padd;
+                ViewBag.pimg = pimg;
+            }
         }
 
         public void showPackages(string fbId_new)
@@ -95,19 +134,57 @@ namespace TourCrow.Controllers
             {
 
                 var pck_name = from pck in packcon.PACKAGEs where pck.UserFBID == fbId_new.ToString() select pck.Title;
-                var check_pack_nm = pck_name.ToList();
-                ViewBag.count_pack = pck_name.Count();
-
-                ViewBag.packages = check_pack_nm;
-
-                //var pck_date = from pck in packcon.PACKAGEs where pck.UserFBID == fbId_new.ToString() select pck.Date;
-                //var check_pack = pck_date.ToList();
-                //ViewBag.count_pack = pck_date.Count();
                 
-                //ViewBag.packages = check_pack;
-            }
+                var check_pack_nm = pck_name.ToList();
+                
+                ViewBag.count_pack = pck_name.Count();
+                //Response.Write(pck_name.Count());
+                ViewBag.packages = check_pack_nm;
+            
+                var pck_id = from pid in packcon.PACKAGEs where pid.UserFBID == fbId_new select pid.PackageID;
+                var check_pid = pck_id.ToList();
+                ViewBag.pid = check_pid;
 
+
+            }
             //return ("Index");
+        }
+
+        public string place_details(string placeId)
+        {
+            string urlAddress = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeId + "&key=" + appKeys.GOOGLE_PLACE_API_KEY;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+
+                return data;
+            }
+            return null;
+        }
+
+        public ProfilePageModel.RootObject parseData(String json)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            ProfilePageModel.RootObject model = js.Deserialize<ProfilePageModel.RootObject>(json);
+            return model;
         }
 
         //[HttpGet]
@@ -184,6 +261,36 @@ namespace TourCrow.Controllers
                     dbcon.SaveChanges();
                 }
             }
+        }
+
+        public ActionResult fileup()
+        {
+            return View("fileup");
+        }
+
+        [HttpPost]
+        public ActionResult fileupload(IEnumerable<HttpPostedFileBase> file)
+        {
+
+            foreach (var f in file)
+            {
+                if (f != null && f.ContentLength > 0 )
+                {
+                    string extension = Path.GetExtension(f.FileName);
+                    var fileName = Path.GetFileName(f.FileName);
+                    //var fileName = f + extension;
+                    var path = Path.Combine(Server.MapPath("~/Content/Album"), fileName);
+                    f.SaveAs(path);
+
+                }
+                else
+                {
+                    ViewBag.PropicError = "Invalid Photo";
+                }
+            }
+
+            //return RedirectToAction("Index", HttpContext.User.Identity.Name.Split('|')[1].ToString());
+            return View("fileup");
         }
 
     }
